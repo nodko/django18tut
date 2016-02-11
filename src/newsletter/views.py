56b5465660ed4d84d05bafc6e125acb9a983c75b
nodko <toktok420@gmail.com>
2016-02-11 20:41:15 +0530

    Changes upto querysets

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .forms import SignUpForm
+from .models import SignUp
 # Create your views here.
 def home(request):
 
-	title = "Welcome"
+	title = "Join Us / Signup Now "
 	
 	# if request.user.is_authenticated():
 	# 	title = "Title context %s" %(request.user)
@@ -20,6 +21,7 @@ def home(request):
 	if form.is_valid():
 		#form.save()
 		#print(request.POST["email"]) # not recomended
+		instance = form.save(commit=False)
 		full_name = form.cleaned_data.get("full_name")
 		if not full_name:
 			full_name = "New full name"
@@ -30,4 +32,19 @@ def home(request):
 		context = {
 	        "template_title":"Thank you"
 	    }
+
+	if request.user.is_authenticated() and request.user.is_staff:
+		# print(SignUp.objects.all())
+		# i = 1
+		# for instances in SignUp.objects.all():
+		# 	print(i)
+		# 	print(instances.full_name)
+		# 	i += 1
+		# queryset = SignUp.objects.all().order_by('-timestamp')
+		queryset = SignUp.objects.all().order_by('-timestamp').filter(full_name__icontains="sagar")
+		# print(SignUp.objects.all().order_by('-timestamp').filter(full_name__icontains="sagar").count())
+		context = {
+		    "queryset": queryset
+		}
+
 	return render(request, "home.html", context)
